bfcedd0928203c00dcb1eacd15682461863c3acb
Mark Petersen <markiepetersen@gmail.com>
2020-03-15 17:13:43 -0600

    first commit

---
+++ code/helloworld/src/main.cpp
@@ -0,0 +1,21 @@
+// Print the words Hello World to the screen, 
+// and waits for user input before terminating. 
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+inline void keep_window_open(){ char ch; cin >> ch;}
+
+int main() {
+
+    cout << "Hello, programming!\n";
+    cout << "Here we go!\n";
+
+    keep_window_open();
+    return 0;
+}
